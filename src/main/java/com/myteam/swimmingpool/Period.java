305dde0b3fc7cf9360f2cae138d64e6862ca9434
admin <>
2018-07-06 08:31:00 +0000

    {/src/main/java/com/myteam/swimmingpool/Period.java}

--- src/main/java/com/myteam/swimmingpool/Period.java
+++ src/main/java/com/myteam/swimmingpool/Period.java
@@ -6,12 +6,33 @@ package com.myteam.swimmingpool;
 
 public class Period implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Period() {
-    }
+	private java.util.Date desiredStartDate;
+	private java.lang.String seasonType;
 
+	public Period() {
+	}
 
+	public java.util.Date getDesiredStartDate() {
+		return this.desiredStartDate;
+	}
 
+	public void setDesiredStartDate(java.util.Date desiredStartDate) {
+		this.desiredStartDate = desiredStartDate;
+	}
+
+	public java.lang.String getSeasonType() {
+		return this.seasonType;
+	}
+
+	public void setSeasonType(java.lang.String seasonType) {
+		this.seasonType = seasonType;
+	}
+
+	public Period(java.util.Date desiredStartDate, java.lang.String seasonType) {
+		this.desiredStartDate = desiredStartDate;
+		this.seasonType = seasonType;
+	}
 
 }
