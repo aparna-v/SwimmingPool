f9c11d74ae9663c59b4e4cc830f662eec231ae59
admin <>
2018-07-06 14:00:58 +0000

    {/src/main/java/com/myteam/swimmingpool/BaseElement.java}

--- src/main/java/com/myteam/swimmingpool/BaseElement.java
+++ src/main/java/com/myteam/swimmingpool/BaseElement.java
@@ -8,7 +8,7 @@ public class BaseElement implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	private java.util.List<com.myteam.swimmingpool.CalculatedAttribute> calculatedAttributeList;
+	private java.util.List<com.myteam.swimmingpool.CalculatedAttribute> calculatedAttributeList = new ArrayList<>();
 
 	public BaseElement() {
 	}
